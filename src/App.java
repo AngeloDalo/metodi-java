c0962b5856458d7a62d6a03d6d6b930a2fcc2c81
AngeloDalo <angelo251158@gmail.com>
2022-05-12 16:58:41 +0200

    overloaded

--- src/App.java
+++ src/App.java
@@ -4,6 +4,9 @@ public class App {
         faiFunzione("ciao2"); 
         int risultato = addizione(5, 3);
         System.out.println(risultato);
+        int sottrazione = sottrazione(2, 1);
+        int sottrazione2 = sottrazione(2, 1, 1);
+        double sottrazione3 = sottrazione(2.2, 1.1);
     }
 
     static void faiFunzione(String cosa){
@@ -13,6 +16,19 @@ public class App {
     static int addizione(int a, int b){
         int risultato = a + b;
         System.out.println(risultato);
+        return risultato;
+    }
+
+    static int sottrazione(int a, int b){
+        int risultato = a - b;
+        return risultato;
+    }
+    static int sottrazione(int a, int b, int c){
+        int risultato = a - b;
+        return risultato;
+    }
+    static double sottrazione(double a, double b){
+        double risultato = a - b;
         return risultato;
     }
 
